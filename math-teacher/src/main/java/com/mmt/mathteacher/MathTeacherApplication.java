3edd71f1285cd7afe8050d58699104d04aedc123
Lee Soyeon <data.sy.2@gmail.com>
2023-06-02 03:10:18 +0900

    spring boot project 생성

---
+++ math-teacher/src/main/java/com/mmt/mathteacher/MathTeacherApplication.java
@@ -0,0 +1,13 @@
+package com.mmt.mathteacher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathTeacherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MathTeacherApplication.class, args);
+	}
+
+}
