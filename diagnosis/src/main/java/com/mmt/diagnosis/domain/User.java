d409b6ae9cdd897a1cbf7299c2098b79c1f8a0a6
Lee Soyeon <data.sy.2@gmail.com>
2023-07-20 22:16:24 +0900

    User 도메인

---
+++ diagnosis/src/main/java/com/mmt/diagnosis/domain/User.java
@@ -0,0 +1,13 @@
+package com.mmt.diagnosis.domain;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+    private String userId;
+    private String userPassword;
+    private String userName;
+    private String userPhone;
+
+}
