6a5d0358b5f0687e7d5c9aca2fded1515b66d723
Lee Soyeon <data.sy.2@gmail.com>
2023-08-02 21:58:49 +0900

    서비스 패키지 만들어서 수정된 부분

--- diagnosis/src/main/java/com/mmt/diagnosis/controller/DiagnosisController.java
+++ diagnosis/src/main/java/com/mmt/diagnosis/controller/DiagnosisController.java
@@ -6,7 +6,7 @@ import com.mmt.diagnosis.dto.student.StudentGetRequest;
 import com.mmt.diagnosis.dto.student.StudentResponse;
 import com.mmt.diagnosis.dto.test.TestResponse;
 import com.mmt.diagnosis.dto.testItem.TestItemsResponse;
-import com.mmt.diagnosis.service.AnswerService;
+import com.mmt.diagnosis.service.answer.AnswerService;
 import com.mmt.diagnosis.service.student.StudentService;
 import com.mmt.diagnosis.service.test.TestService;
 import com.mmt.diagnosis.service.testItem.TestItemService;
@@ -64,6 +64,7 @@ public class DiagnosisController {
 
     /**
      * 진단 학습지 상세보기 : 선택한 student_id와 test_id에 따른 진단 학습지 생성
+     * 리팩토링 : 같은 학생, 같은 학습지를 선택했을 시 '재시험'이라는 것을 명시해주는 알람 띄우는 검증 로직 추가하기
      */
     @GetMapping("/diagnosis/diagnostic-test")
     public DiagnosticTestResponse getDiagnosticTest(@RequestBody DiagnosticTestRequest request){
