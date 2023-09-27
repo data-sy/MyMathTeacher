e86eb232f74b82825d964835e63d568f7996839b
Lee Soyeon <data.sy.2@gmail.com>
2023-09-27 14:15:19 +0900

    crud 테스트
    - 스프링 서버와 api 통신
    - students로

--- mmt-web/vue.config.js
+++ mmt-web/vue.config.js
@@ -1,4 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // 멀티워드 아니어도 되게
+  lintOnSave: false
 })
