3aa417cb74ed8b7100d553919421779ee1a36ece
Lee Soyeon <data.sy.2@gmail.com>
2023-11-16 08:43:43 +0900

    sql 정리

--- diagnosis/sql/select.sql
+++ diagnosis/sql/select.sql
@@ -159,4 +159,9 @@ SELECT concept_id, skill_id FROM concepts WHERE concept_id in (4079, 4077, 4104,
 -- 2학기 중간고사
 SELECT concept_id, skill_id FROM concepts WHERE concept_id in (11);
 
--- 학습지 
+-- 학습지
+select * from probabilities p 
+JOIN answers a ON p.answer_id = a.answer_id
+where student_test_id = 10;
+
+select * from concepts where concept_id in (select concept_id from probabilities where to_concept_depth=1 and answer_id in (157, 160, 163, 165, 167, 169 ))
